4833cabb4eb9d1ad66850445ca119fdbf94132d0
Tomas Perez <tomasitopg6@gmail.com>
2020-08-06 16:28:18 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,5 +2,14 @@ class Dog
 
   attr_accessor :name, :breed, :age
 
-  
+  @@all = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+
+  def self.all
 end
